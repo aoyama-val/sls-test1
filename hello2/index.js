f57af6442eb8e752b9998acc37ed573ac88346c7
Aoyama, Shotaro <aoyama@val.co.jp>
2019-04-12 06:55:30 +0900

    update hello2

--- hello2/index.js
+++ hello2/index.js
@@ -1,15 +1,23 @@
 'use strict';
 
+const axios = require('axios');
+
 exports.handler = async (event) => {
     console.log("This is console.log");
     console.error("This is console.error");
+
+    const params = {
+        a: 'hoge',
+        b: 123,
+    };
+    let res = await axios.get('http://httpbin.org/get', { params: params });
+
     const response = {
         statusCode: 200,
         body: JSON.stringify({
-            message: 'Hello from Lambda!',
-            event: event,
+            message: 'axios test',
+            result: res.data,
         }),
-        hoge: "modified6",
     };
     return response;
 };
